c31eb8841891f687b06137ec4c4e5b9041c8226a
eliav <eliav@autocompleteplus.com>
2022-07-19 18:01:02 +0300

    Add Fast Simon Autocomplete search

--- README.md
+++ README.md
@@ -1,46 +1,63 @@
-# Hydrogen Demo Store
+# Fast Simon & Shopify Integration
+
+Build Search and Discovery experience with Fast Simon, a Shopify Plus Certified Partner
+
+[Getting started guide](https://instantsearchplus.zendesk.com/hc/en-us/categories/360000839131-Getting-Started-with-Fast-Simon)
+
+# Hydrogen App
 
 Hydrogen is a React framework and SDK that you can use to build fast and dynamic Shopify custom storefronts.
 
 [Check out the docs](https://shopify.dev/custom-storefronts/hydrogen)
 
-[Run this template on StackBlitz](https://stackblitz.com/github/Shopify/hydrogen/tree/stackblitz/templates/demo-store)
 
-## Getting started
+### Configuration for Hydrogen & Fast Simon
+
+* Hydrogen
+
+Update `hydrogen.config.js` with your shop's domain and Storefront API token.
+
+* Fast Simon
+
+Update fastsimon.config.json with your Fast Simon UUID & store id.
+
+
+### Template features
+
+* Fast Simon Autocomplete search
+  
+* End-to-end testing with [Playwright](https://playwright.dev)
+
+* Unit testing with [Vitest](https://vitest.dev) and [Testing Library](https://testing-library.com)
+
+* Code formatting with [Prettier](https://prettier.io)
+
+* Javascript linting with [ESLint](https://eslint.org) and the Hydrogen [ESLint plugin](https://github.com/Shopify/hydrogen/tree/main/packages/eslint-plugin)
+
+
+### Getting started
 
 **Requirements:**
 
 - Node.js version 16.5.0 or higher
 - Yarn
 
-To create a new Hydrogen app, run:
-
 ```bash
-npm init @shopify/hydrogen
+yarn
+yarn dev
 ```
 
-## Running the dev server
+### Previewing a production build
 
-Then `cd` into the new directory and run:
+To run a local preview of your Hydrogen app in an environment similar to Oxygen, build your Hydrogen app and then run `yarn preview`:
 
 ```bash
-npm install
-npm run dev
+yarn build
+yarn preview
 ```
 
-Remember to update `hydrogen.config.js` with your shop's domain and Storefront API token!
-
-## Building for production
+### Building for production
 
 ```bash
-npm run build
-```
-
-## Previewing a production build
-
-To run a local preview of your Hydrogen app in an environment similar to Oxygen, build your Hydrogen app and then run `npm run preview`:
-
-```bash
-npm run build
-npm run preview
+yarn build
 ```
--- src/components/global/Header.client.tsx
+++ src/components/global/Header.client.tsx
@@ -1,5 +1,6 @@
 import {Link, useUrl, useCart} from '@shopify/hydrogen';
 import {useWindowScroll} from 'react-use';
+import {useEffect} from "react";
 
 import {
   Heading,
@@ -15,6 +16,8 @@ import {MenuDrawer} from './MenuDrawer.client';
 import {useDrawer} from './Drawer.client';
 
 import type {EnhancedMenu} from '~/lib/utils';
+
+import fastSimonConfig from '../../../fastsimon.config.json';
 
 /**
  * A client component that specifies the content of the header on the website
@@ -39,35 +42,40 @@ export function Header({title, menu}: {title: string; menu?: EnhancedMenu}) {
     closeDrawer: closeMenu,
   } = useDrawer();
 
+  useEffect(() => {
+      import('@fast-simon/hydrogen-autocomplete')
+          .then(Module => Module.initializeFastSimonAutocomplete(fastSimonConfig.UUID, fastSimonConfig.storeID, fastSimonConfig.shop))
+  }, []);
+
   return (
-    <>
-      <CartDrawer isOpen={isCartOpen} onClose={closeCart} />
-      <MenuDrawer isOpen={isMenuOpen} onClose={closeMenu} menu={menu!} />
-      <DesktopHeader
-        countryCode={countryCode}
-        isHome={isHome}
-        title={title}
-        menu={menu}
-        openCart={openCart}
-      />
-      <MobileHeader
-        countryCode={countryCode}
-        isHome={isHome}
-        title={title}
-        openCart={openCart}
-        openMenu={openMenu}
-      />
-    </>
+      <>
+        <CartDrawer isOpen={isCartOpen} onClose={closeCart} />
+        <MenuDrawer isOpen={isMenuOpen} onClose={closeMenu} menu={menu!} />
+        <DesktopHeader
+            countryCode={countryCode}
+            isHome={isHome}
+            title={title}
+            menu={menu}
+            openCart={openCart}
+        />
+        <MobileHeader
+            countryCode={countryCode}
+            isHome={isHome}
+            title={title}
+            openCart={openCart}
+            openMenu={openMenu}
+        />
+      </>
   );
 }
 
 function MobileHeader({
-  countryCode,
-  title,
-  isHome,
-  openCart,
-  openMenu,
-}: {
+                        countryCode,
+                        title,
+                        isHome,
+                        openCart,
+                        openMenu,
+                      }: {
   countryCode?: string | null;
   title: string;
   isHome: boolean;
@@ -79,70 +87,71 @@ function MobileHeader({
   const styles = {
     button: 'relative flex items-center justify-center w-8 h-8',
     container: `${
-      isHome
-        ? 'bg-primary/80 dark:bg-contrast/60 text-contrast dark:text-primary shadow-darkHeader'
-        : 'bg-contrast/80 text-primary'
+        isHome
+            ? 'bg-primary/80 dark:bg-contrast/60 text-contrast dark:text-primary shadow-darkHeader'
+            : 'bg-contrast/80 text-primary'
     } ${
-      y > 50 && !isHome ? 'shadow-lightHeader ' : ''
+        y > 50 && !isHome ? 'shadow-lightHeader ' : ''
     }flex lg:hidden items-center h-nav sticky backdrop-blur-lg z-40 top-0 justify-between w-full leading-none gap-4 px-4 md:px-8`,
   };
 
   return (
-    <header role="banner" className={styles.container}>
-      <div className="flex items-center justify-start w-full gap-4">
-        <button onClick={openMenu} className={styles.button}>
-          <IconMenu />
-        </button>
-        <form
-          action={`/${countryCode ? countryCode + '/' : ''}search`}
-          className="items-center gap-2 sm:flex"
-        >
-          <button type="submit" className={styles.button}>
-            <IconSearch />
+      <header role="banner" className={styles.container}>
+        <div className="flex items-center justify-start w-full gap-4">
+          <button onClick={openMenu} className={styles.button}>
+            <IconMenu />
           </button>
-          <Input
-            className={
-              isHome
-                ? 'focus:border-contrast/20 dark:focus:border-primary/20'
-                : 'focus:border-primary/20'
-            }
-            type="search"
-            variant="minisearch"
-            placeholder="Search"
-            name="q"
-          />
-        </form>
-      </div>
+          <form
+              action={`/${countryCode ? countryCode + '/' : ''}search`}
+              className="items-center gap-2 sm:flex"
+              autoComplete="off"
+          >
+            <button type="submit" className={styles.button}>
+              <IconSearch />
+            </button>
+            <Input
+                className={
+                  isHome
+                      ? 'focus:border-contrast/20 dark:focus:border-primary/20'
+                      : 'focus:border-primary/20'
+                }
+                type="search"
+                variant="minisearch"
+                placeholder="Search"
+                name="q"
+            />
+          </form>
+        </div>
 
-      <Link
-        className="flex items-center self-stretch leading-[3rem] md:leading-[4rem] justify-center flex-grow w-full h-full"
-        to="/"
-      >
-        <Heading className="font-bold text-center" as={isHome ? 'h1' : 'h2'}>
-          {title}
-        </Heading>
-      </Link>
-
-      <div className="flex items-center justify-end w-full gap-4">
-        <Link to={'/account'} className={styles.button}>
-          <IconAccount />
+        <Link
+            className="flex items-center self-stretch leading-[3rem] md:leading-[4rem] justify-center flex-grow w-full h-full"
+            to="/"
+        >
+          <Heading className="font-bold text-center" as={isHome ? 'h1' : 'h2'}>
+            {title}
+          </Heading>
         </Link>
-        <button onClick={openCart} className={styles.button}>
-          <IconBag />
-          <CartBadge dark={isHome} />
-        </button>
-      </div>
-    </header>
+
+        <div className="flex items-center justify-end w-full gap-4">
+          <Link to={'/account'} className={styles.button}>
+            <IconAccount />
+          </Link>
+          <button onClick={openCart} className={styles.button}>
+            <IconBag />
+            <CartBadge dark={isHome} />
+          </button>
+        </div>
+      </header>
   );
 }
 
 function DesktopHeader({
-  countryCode,
-  isHome,
-  menu,
-  openCart,
-  title,
-}: {
+                         countryCode,
+                         isHome,
+                         menu,
+                         openCart,
+                         title,
+                       }: {
   countryCode?: string | null;
   isHome: boolean;
   openCart: () => void;
@@ -153,60 +162,61 @@ function DesktopHeader({
 
   const styles = {
     button:
-      'relative flex items-center justify-center w-8 h-8 focus:ring-primary/5',
+        'relative flex items-center justify-center w-8 h-8 focus:ring-primary/5',
     container: `${
-      isHome
-        ? 'bg-primary/80 dark:bg-contrast/60 text-contrast dark:text-primary shadow-darkHeader'
-        : 'bg-contrast/80 text-primary'
+        isHome
+            ? 'bg-primary/80 dark:bg-contrast/60 text-contrast dark:text-primary shadow-darkHeader'
+            : 'bg-contrast/80 text-primary'
     } ${
-      y > 50 && !isHome ? 'shadow-lightHeader ' : ''
+        y > 50 && !isHome ? 'shadow-lightHeader ' : ''
     }hidden h-nav lg:flex items-center sticky transition duration-300 backdrop-blur-lg z-40 top-0 justify-between w-full leading-none gap-8 px-12 py-8`,
   };
 
   return (
-    <header role="banner" className={styles.container}>
-      <div className="flex gap-12">
-        <Link className={`font-bold`} to="/">
-          {title}
-        </Link>
-        <nav className="flex gap-8">
-          {/* Top level menu items */}
-          {(menu?.items || []).map((item) => (
-            <Link key={item.id} to={item.to} target={item.target}>
-              {item.title}
-            </Link>
-          ))}
-        </nav>
-      </div>
-      <div className="flex items-center gap-1">
-        <form
-          action={`/${countryCode ? countryCode + '/' : ''}search`}
-          className="flex items-center gap-2"
-        >
-          <Input
-            className={
-              isHome
-                ? 'focus:border-contrast/20 dark:focus:border-primary/20'
-                : 'focus:border-primary/20'
-            }
-            type="search"
-            variant="minisearch"
-            placeholder="Search"
-            name="q"
-          />
-          <button type="submit" className={styles.button}>
-            <IconSearch />
+      <header role="banner" className={styles.container}>
+        <div className="flex gap-12">
+          <Link className={`font-bold`} to="/">
+            {title}
+          </Link>
+          <nav className="flex gap-8">
+            {/* Top level menu items */}
+            {(menu?.items || []).map((item) => (
+                <Link key={item.id} to={item.to} target={item.target}>
+                  {item.title}
+                </Link>
+            ))}
+          </nav>
+        </div>
+        <div className="flex items-center gap-1">
+          <form
+              action={`/${countryCode ? countryCode + '/' : ''}search`}
+              className="flex items-center gap-2"
+              autoComplete="off"
+          >
+            <Input
+                className={
+                  isHome
+                      ? 'focus:border-contrast/20 dark:focus:border-primary/20'
+                      : 'focus:border-primary/20'
+                }
+                type="search"
+                variant="minisearch"
+                placeholder="Search"
+                name="q"
+            />
+            <button type="submit" className={styles.button}>
+              <IconSearch />
+            </button>
+          </form>
+          <Link to={'/account'} className={styles.button}>
+            <IconAccount />
+          </Link>
+          <button onClick={openCart} className={styles.button}>
+            <IconBag />
+            <CartBadge dark={isHome} />
           </button>
-        </form>
-        <Link to={'/account'} className={styles.button}>
-          <IconAccount />
-        </Link>
-        <button onClick={openCart} className={styles.button}>
-          <IconBag />
-          <CartBadge dark={isHome} />
-        </button>
-      </div>
-    </header>
+        </div>
+      </header>
   );
 }
 
@@ -217,14 +227,14 @@ function CartBadge({dark}: {dark: boolean}) {
     return null;
   }
   return (
-    <div
-      className={`${
-        dark
-          ? 'text-primary bg-contrast dark:text-contrast dark:bg-primary'
-          : 'text-contrast bg-primary'
-      } absolute bottom-1 right-1 text-[0.625rem] font-medium subpixel-antialiased h-3 min-w-[0.75rem] flex items-center justify-center leading-none text-center rounded-full w-auto px-[0.125rem] pb-px`}
-    >
-      <span>{totalQuantity}</span>
-    </div>
+      <div
+          className={`${
+              dark
+                  ? 'text-primary bg-contrast dark:text-contrast dark:bg-primary'
+                  : 'text-contrast bg-primary'
+          } absolute bottom-1 right-1 text-[0.625rem] font-medium subpixel-antialiased h-3 min-w-[0.75rem] flex items-center justify-center leading-none text-center rounded-full w-auto px-[0.125rem] pb-px`}
+      >
+        <span>{totalQuantity}</span>
+      </div>
   );
 }
